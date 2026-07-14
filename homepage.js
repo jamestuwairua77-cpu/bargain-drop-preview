2bf852a7608d13ac04d27bca937ca814d2b5d544
jamestuwairua77-cpu <jamestuwairua77@gmail.com>
2026-07-14 03:19:30 -0400

    fix: pin CDN to 312d865 for body_html image fixes

--- homepage.js
+++ homepage.js
@@ -27,7 +27,7 @@ x.send();
 // Load trending products from CDN (first category)
 (function loadProds(){
 var x=new XMLHttpRequest();
-x.open('GET','https://cdn.jsdelivr.net/gh/jamestuwairua77-cpu/bargain-drop-preview@f352b5463eb3047f4e1e436f736e62af75adfc06/data/home-garden-furniture.json',true);
+x.open('GET','https://cdn.jsdelivr.net/gh/jamestuwairua77-cpu/bargain-drop-preview@312d8650f66b8587f14173ab7ab23c4fb0c4f9b6/data/home-garden-furniture.json',true);
 x.timeout=15000;
 x.onload=function(){
   if(x.status>=200&&x.status<400){
